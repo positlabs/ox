1449632e96ab915d59d8559ce3fc656e0dfd88dc
Josh <josh@toolofna.com>
2014-02-17 13:56:33 -0800

    build

--- ox.js
+++ ox.js
@@ -477,6 +477,52 @@ define('Events',['require'],function (require) {
 	return Events;
 
 });
+define('DOMEvents',['require'],function (require) {
+
+	var DOMEvents = function (element) {
+
+		element.on = function (event, callback) {
+			element.addEventListener(event, callback);
+			return callback;
+		};
+
+		element.once = function (event, callback) {
+			element.on(event, function (data) {
+				element.off(event, callback);
+				callback(data);
+			});
+		};
+
+		element.off = function (event, callback) {
+			element.removeEventListener(event, callback);
+		};
+
+		element.trigger = function (eventName) {
+
+			var event; // The custom event that will be created
+
+			if (document.createEvent) {
+				event = document.createEvent("HTMLEvents");
+				event.initEvent(eventName, true, true);
+			} else {
+				event = document.createEventObject();
+				event.eventType = eventName;
+			}
+
+			event.eventName = eventName;
+
+			if (document.createEvent) {
+				element.dispatchEvent(event);
+			} else {
+				element.fireEvent("on" + event.eventType, event);
+			}
+		}
+
+	};
+
+	return DOMEvents;
+
+});
 define('ReqAnimFrame',['require'],function (require) {
 	
 
@@ -537,7 +583,7 @@ define('FrameImpulse',['require','ReqAnimFrame','Events'],function (require) {
 	return FrameImpulse;
 
 });
-define('ox',['require','Events','FrameImpulse'],function (require) {
+define('ox',['require','Events','DOMEvents','FrameImpulse'],function (require) {
 
 	/**
 
@@ -647,6 +693,7 @@ define('ox',['require','Events','FrameImpulse'],function (require) {
 	};
 
 	ox.Events = require("Events");
+	ox.DOMEvents = require("DOMEvents");
 	ox.FrameImpulse = require("FrameImpulse");
 
 
